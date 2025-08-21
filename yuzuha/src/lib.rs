352df7a768cf485cead3bae26791acf85e26303e
Conch116 <437087748@qq.com>
2025-08-21 17:41:44 +0800

    patch

--- yuzuha/src/lib.rs
+++ yuzuha/src/lib.rs
@@ -15,33 +15,54 @@ mod util;
 
 type PtrToStringAnsi = extern "C" fn(chars: *const u8) -> u64;
 static BASE: OnceLock<usize> = OnceLock::new();
+static QT5_CORE_BASE: OnceLock<usize> = OnceLock::new();
 
 fn on_attach() {
     // SAFETY: fuck off
-    thread::sleep(Duration::from_secs(3));
-    unsafe {
+    thread::sleep(Duration::from_secs(3)); //如果控制台不打印信息,代码也会正常执行
+    unsafe {                               //能在客户端发送请求之前 hook 成功即可
         let _ = Console::FreeConsole();
         let _ = Console::AllocConsole();
     }
-
     println!("yuzuha-patch (For Blue Genshin) is initializing");
 
     let base = loop {
         unsafe {
             match GetModuleHandleA(s!("GameAssembly.dll")) {
-                Ok(handle) => break handle.0 as usize,
+                Ok(handle) => {
+                    let base_addr = handle.0 as usize;
+                    println!("GameAssembly.dll Addr: 0x{:X}", base_addr);
+                    break base_addr;
+                },
+                Err(_) => thread::sleep(Duration::from_millis(200)),
+            }
+        }
+    };
+    
+    let _qt5_core_base = loop {
+        unsafe {
+            match GetModuleHandleA(s!("Qt5Core.dll")) {
+                Ok(handle) => {
+                    let base_addr = handle.0 as usize;
+                    println!("Qt5Core.dll Addr: 0x{:X}", base_addr);
+                    break base_addr;
+                },
                 Err(_) => thread::sleep(Duration::from_millis(200)),
             }
         }
     };
 
-    thread::sleep(Duration::from_secs(5));
+    //thread::sleep(Duration::from_secs(5)); 不要过多使用sleep,要在客户端发送请求前hook成功
     util::disable_memory_protection();
 
     let _ = BASE.set(base);
-    let mut interceptor = Interceptor::new(base);
+    let _ = QT5_CORE_BASE.set(_qt5_core_base);
 
-    network::hook_make_initial_url(&mut interceptor);
+    let mut interceptor = Interceptor::new(base);
+    let mut interceptor_qurl = Interceptor::new(_qt5_core_base);
+
+    network::hook_info_manager_set_api_url(&mut interceptor);
+    network::hook_qt5_core_qurl(&mut interceptor_qurl);
 
     std::thread::sleep(Duration::from_secs(u64::MAX));
 }
--- yuzuha/src/network.rs
+++ yuzuha/src/network.rs
@@ -4,33 +4,58 @@ use yuzuha_codegen::use_offsets;
 
 use crate::{BASE, PtrToStringAnsi, interceptor::Interceptor, util};
 
-#[use_offsets(MAKE_INITIAL_URL, PTR_TO_STRING_ANSI)]
-pub fn hook_make_initial_url(interceptor: &mut Interceptor) {
+#[use_offsets(INFOMANAGER_SET_APIURL, PTR_TO_STRING_ANSI)]
+pub fn hook_info_manager_set_api_url(interceptor: &mut Interceptor) {
+    const MANJUU_LOCAL: &str = "http://127.0.0.1:5000";
+    interceptor.attach(INFOMANAGER_SET_APIURL, |ctx| {
+        let url = util::read_csharp_string(ctx.registers().rcx as *const u8);
+        let new_url = MANJUU_LOCAL.to_string();
+        let ptr_to_string_ansi = unsafe {
+            std::mem::transmute::<usize, PtrToStringAnsi>(BASE.get().unwrap() + PTR_TO_STRING_ANSI)
+        };
+        println!("UnityWebRequest: \"{url}\", replacing with \"{new_url}\" (manjuu redirect)");
+        ctx.registers().rcx = ptr_to_string_ansi(
+            CString::new(new_url.as_str())
+                .unwrap()
+                .to_bytes_with_nul()
+                .as_ptr(),
+            );
+    });
+}
+
+#[use_offsets(QT5_CORE_QURL)]
+pub fn hook_qt5_core_qurl(interceptor: &mut Interceptor) {
     const MANJUU_LOCAL: &str = "http://127.0.0.1:5000";
 
-    interceptor.attach(MAKE_INITIAL_URL, |ctx| {
-        let url = util::read_csharp_string(ctx.registers().rcx as *const u8);
+    interceptor.attach(QT5_CORE_QURL, |ctx| {
+        let ptr = unsafe {
+            *(ctx.registers().rdx as *const *const u8) // 解一次引用
+        };
+        let url = util::read_qurl_string(ptr);
         println!("Intercepted URL: {}", url);
-
-        // 仅替换 manjuu.com 到本地的逻辑（保留原路径）
-        if url.contains("manjuu.com") && !url.contains("project-3") && !url.contains("launcher") {
+        
+        if !url.is_empty()  && url.contains("manjuu.com") {
             let mut new_url = MANJUU_LOCAL.to_string();
+            println!("UnityWebRequest: \"{url}\", replacing with \"{new_url}\" (manjuu redirect)");
             url.split('/').skip(3).for_each(|s| {
                 new_url.push('/');
                 new_url.push_str(s);
             });
+            let new_url_utf16: Vec<u16> = new_url.encode_utf16().collect(); // utf-8 -> utf-16
+            if new_url_utf16.len() <= url.len() {
+                unsafe {
+                    let url_str = ptr.byte_offset(0x18).cast::<u16>().cast_mut();
+                    *ptr.byte_offset(0x4).cast::<u32>().cast_mut() = new_url_utf16.len() as u32; // str_len
+                    *ptr.byte_offset(0x8).cast::<u32>().cast_mut() = (new_url_utf16.len() + 1) as u32; // str_len + 1
 
-            println!("UnityWebRequest: \"{url}\", replacing with \"{new_url}\" (manjuu redirect)");
-            let ptr_to_string_ansi = unsafe {
-                std::mem::transmute::<usize, PtrToStringAnsi>(BASE.get().unwrap() + PTR_TO_STRING_ANSI)
-            };
-
-            ctx.registers().rcx = ptr_to_string_ansi(
-                CString::new(new_url.as_str())
-                    .unwrap()
-                    .to_bytes_with_nul()
-                    .as_ptr(),
-            );
+                    std::ptr::write_bytes(url_str, 0, url.len());
+                    std::ptr::copy_nonoverlapping(
+                        new_url_utf16.as_ptr(),
+                        url_str,
+                        new_url_utf16.len(),
+                    );
+                }
+            } 
         }
     });
 }
